cfb222231f8c8d18c3567d247aa1a32a728f425f
Yashi Gupta <yashig406@gmail.com>
2025-03-29 16:11:18 +0530

    SetUp

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Customizer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Customizer
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,html}', 
+    './public/index.html',            
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
