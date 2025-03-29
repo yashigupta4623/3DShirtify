713a5f061894d3751b0f0c12ae6dd0d456ca8b49
Yashi Gupta <yashig406@gmail.com>
2025-03-29 23:29:00 +0530

    HomePage

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Customizer = () => {
   return (
     <div>
-      
+      dfjn
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,56 @@
-import {motion, AnimatePresence} from 'framer-motion';
-import {useSnapshot} from 'valtio';
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  slideAnimation,
+  headTextAnimation,
+} from "../config/motion.js";
+import {CustomButton} from "../components";
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default Home
+  console.log("Home Component Loaded"); // Debugging line
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain" />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContentAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.
+              </h1>
+              </motion.div>
+
+              <motion.div {...headContainerAnimation} className="flex flex-col gap-5">
+                <p className="max-w-md font-normal text-gray-600 text-base">
+                  Create your unique and exclusive shirt with our brand-new 3D customization tool.
+                  <strong> Unleash your imagination</strong>{" "} andnd define your own style.
+                </p>
+
+                <CustomButton
+                  type="filled"
+                  title="Customize It"
+                  handleClick={() => (state.intro = false)}
+                  customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                  />
+
+              </motion.div>
+          </motion.div>
+        </motion.section>
+
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
