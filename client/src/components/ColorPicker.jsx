97f9659ab4d6465a750c30c6ac172b305811045c
Yashi Gupta <yashig406@gmail.com>
2025-03-30 17:47:56 +0530

    adding colorPicker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { SketchPicker } from "react-color";
 
 const ColorPicker = () => {
-  return (
-    <div>
-      ColorPicker
-    </div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default ColorPicker
+  console.log("Current color:", snap.color); // Debugging
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        // to add by default
+        // presetColors={[ "#FF5733", "#DAF7A6","#FFC0CB","#00FFFF", "#FF00FF","#FFA500","#FFFFFF","#800080","#008000", "#0000FF","#FF0000","#000000"]}
+        onChange={(color) => {
+          console.log("Selected color:", color.hex); // Debugging
+          state.color = color.hex;
+        }}
+      />
+    </div>
+  );
+};
+
+export default ColorPicker;
